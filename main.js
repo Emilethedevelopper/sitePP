0d2f19f4f411855aa634195f8f250b58948c322e
Emilethedevelopper <44734358+Emilethedevelopper@users.noreply.github.com>
2018-11-03 16:39:22 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var modal= document.getElementById('modal');
+
+var modalBtn= document.getElementById("style");
+
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+modalBtn.addEventListener("click", openModal);
+
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target == modal){
+    modal.style.display = 'none';
+  }
+}
